b6e49d5ca9e0cda9f5095959daee76d2e4ded9c4
Alain Benbassat <alain@businessandcode.eu>
2020-03-19 14:35:00 +0100

    first version

---
+++ README.md
@@ -0,0 +1,7 @@
+# be.vivasalud.vivamailings
+
+Custom stuff with mailings.
+
+## disable URL tracking
+When a mailing is saved, url tracking is turned off.
+
---
+++ vivamailings.php
@@ -0,0 +1,181 @@
+<?php
+
+require_once 'vivamailings.civix.php';
+use CRM_Vivamailings_ExtensionUtil as E;
+
+function vivamailings_civicrm_pre($op, $objectName, $id, &$params) {
+  // disable URL tracking
+  if ($objectName == 'Mailing') {
+    if ($op == 'edit' || $op == 'create') {
+      if (array_key_exists('url_tracking', $params)) {
+        $params['url_tracking'] = 0;
+      }
+    }
+  }
+}
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_config/
+ */
+function vivamailings_civicrm_config(&$config) {
+  _vivamailings_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_xmlMenu().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_xmlMenu
+ */
+function vivamailings_civicrm_xmlMenu(&$files) {
+  _vivamailings_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
+ */
+function vivamailings_civicrm_install() {
+  _vivamailings_civix_civicrm_install();
+}
+
+/**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
+ */
+function vivamailings_civicrm_postInstall() {
+  _vivamailings_civix_civicrm_postInstall();
+}
+
+/**
+ * Implements hook_civicrm_uninstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
+ */
+function vivamailings_civicrm_uninstall() {
+  _vivamailings_civix_civicrm_uninstall();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
+ */
+function vivamailings_civicrm_enable() {
+  _vivamailings_civix_civicrm_enable();
+}
+
+/**
+ * Implements hook_civicrm_disable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
+ */
+function vivamailings_civicrm_disable() {
+  _vivamailings_civix_civicrm_disable();
+}
+
+/**
+ * Implements hook_civicrm_upgrade().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
+ */
+function vivamailings_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _vivamailings_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implements hook_civicrm_managed().
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_managed
+ */
+function vivamailings_civicrm_managed(&$entities) {
+  _vivamailings_civix_civicrm_managed($entities);
+}
+
+/**
+ * Implements hook_civicrm_caseTypes().
+ *
+ * Generate a list of case-types.
+ *
+ * Note: This hook only runs in CiviCRM 4.4+.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_caseTypes
+ */
+function vivamailings_civicrm_caseTypes(&$caseTypes) {
+  _vivamailings_civix_civicrm_caseTypes($caseTypes);
+}
+
+/**
+ * Implements hook_civicrm_angularModules().
+ *
+ * Generate a list of Angular modules.
+ *
+ * Note: This hook only runs in CiviCRM 4.5+. It may
+ * use features only available in v4.6+.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_angularModules
+ */
+function vivamailings_civicrm_angularModules(&$angularModules) {
+  _vivamailings_civix_civicrm_angularModules($angularModules);
+}
+
+/**
+ * Implements hook_civicrm_alterSettingsFolders().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_alterSettingsFolders
+ */
+function vivamailings_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
+  _vivamailings_civix_civicrm_alterSettingsFolders($metaDataFolders);
+}
+
+/**
+ * Implements hook_civicrm_entityTypes().
+ *
+ * Declare entity types provided by this module.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
+ */
+function vivamailings_civicrm_entityTypes(&$entityTypes) {
+  _vivamailings_civix_civicrm_entityTypes($entityTypes);
+}
+
+/**
+ * Implements hook_civicrm_thems().
+ */
+function vivamailings_civicrm_themes(&$themes) {
+  _vivamailings_civix_civicrm_themes($themes);
+}
+
+// --- Functions below this ship commented out. Uncomment as required. ---
+
+/**
+ * Implements hook_civicrm_preProcess().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_preProcess
+ *
+function vivamailings_civicrm_preProcess($formName, &$form) {
+
+} // */
+
+/**
+ * Implements hook_civicrm_navigationMenu().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_navigationMenu
+ *
+function vivamailings_civicrm_navigationMenu(&$menu) {
+  _vivamailings_civix_insert_navigation_menu($menu, 'Mailings', array(
+    'label' => E::ts('New subliminal message'),
+    'name' => 'mailing_subliminal_message',
+    'url' => 'civicrm/mailing/subliminal',
+    'permission' => 'access CiviMail',
+    'operator' => 'OR',
+    'separator' => 0,
+  ));
+  _vivamailings_civix_navigationMenu($menu);
+} // */
